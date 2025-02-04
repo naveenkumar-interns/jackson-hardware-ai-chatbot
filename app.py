53c12316f051916f954345d511c108a600f954d7
naveenkumar-interns <naveenkumar.intern@gmail.com>
2025-02-04 21:42:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -351,6 +351,9 @@ def clear_chat():
 def home():
     return jsonify({"message": "working"})
 
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 10000))  # Render assigns a dynamic port
+#     app.run(host="0.0.0.0", port=port)
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render assigns a dynamic port
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
